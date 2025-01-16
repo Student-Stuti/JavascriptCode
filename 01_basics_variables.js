f7b8a55289e1b6b677b2454d5ec409ded93bcbdc
Student Stuti <stutisharma8912@gmail.com>
2025-01-16 22:55:06 +0530

    added 2 files

---
+++ 01_basics_variables.js
@@ -0,0 +1,41 @@
+const accountId = 122323;
+let accountEmail = "stuti@google.com";
+var accountPassword = "123456";
+accountCity = "Jaipur";   // it is not suggested to declare a variable like this 
+let accountState ;   // if we do not give any value then it will give output undefined
+
+// accountId = 23454  not allowed const value cannot be updated
+
+
+
+console.log(accountId);
+
+/*
+Do not use var bacause
+var is global scope variable
+- var can be redeclared and updated
+- if two different files have var intialized variables with same name and we 
+update the value of variable in any file then it is updated in all the files bacause
+it is global scope variable
+
+let is block scope variable 
+- let cannot be redeclared but can be updated 
+
+var was used till 2015 after that ES6 (Ecmascript 6) which introduce let and const 
+*/
+
+
+accountEmail = "stuti@gmail.com";
+accountPassword = "345678";
+accountCity = "Mohali";
+
+console.table([accountEmail, accountPassword, accountCity ,accountState]);
+
+/* 
+(index) │      Values       │
+├─────────┼───────────────────┤
+│    0    │ 'stuti@gmail.com' │
+│    1    │     '345678'      │
+│    2    │     'Mohali'      │
+│    3    │     undefined 
+*/
---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all code as newest version of code
+
+//alert("hello")  we cannot use alert here because it is node not browser
+
+let name = "abc"; //string
+let age = 12; //int
+let isLogged = true; //boolean
+let state; // if we console this value then output will be undefined
+
+// PRIMITIVE DATA TYPES
+//number 2 - power53
+//bigint
+//string  ""
+//boolean  true/false
+//undefined where value is not assigned yet
+//symbol  unique
+
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
