e32462bdd9c57562a2c1e30d17b2871c3d8f3768
Student Stuti <stutisharma8912@gmail.com>
2025-01-17 13:47:06 +0530

    added conversion file

---
+++ 03_conversion_operation.js
@@ -0,0 +1,40 @@
+let score = "33";
+//console.log(typeof score); //string
+
+// let score1 = "33abc";
+// let valueNumber = Number(score1);
+// console.log(typeof valueNumber); //number
+// console.log(valueNumber); // NaN
+
+// let score1 = null
+//let valueNumber = Number(score1);
+//console.log(typeof valueNumber); //number
+//console.log(valueNumber); // 0
+
+// let score1 = undefined
+//console.log(typeof valueNumber); //number
+//console.log(valueNumber); // NaN
+
+//true=>1  false => 0
+
+// let loggedIn = "";
+// let booleanLoggedIn = Boolean(loggedIn);
+// console.log(typeof booleanLoggedIn);  //boolean
+// console.log(booleanLoggedIn); //false
+
+// let loggedIn = "Stuti";
+// let booleanLoggedIn = Boolean(loggedIn);
+// console.log(typeof booleanLoggedIn); //boolean
+// console.log(booleanLoggedIn); //true
+
+// let someNumber =33;
+// let stringValue = String(someNumber);
+// console.log(typeof stringValue); //string
+// console.log(stringValue); //33
+
+//****************************Operations***************************************** */
+
+console.log("1" + 2); //12
+console.log(2 + "1"); //21
+console.log("1" + "2" + "3"); //123
+console.log(2 + 1 + "3"); //33 ***this is special case ****
