cb970c40b4d46c7a9f22c05106705dd6416819ba
Student Stuti <stutisharma8912@gmail.com>
2025-01-18 12:52:53 +0530

    modified datatype file

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -15,6 +15,40 @@ let state; // if we console this value then output will be undefined
 //undefined where value is not assigned yet
 //symbol  unique
 
-console.log(typeof age); //number
-console.log(typeof null); //object
-console.log(typeof undefined); //undefined
+// console.log(typeof age); //number
+// console.log(typeof null); //object
+// console.log(typeof undefined); //undefined
+
+/* 
+Primitive DataTypes(7) => Number String Boolean Undefined Null BigInt Symbol
+
+Non Primitive DataTypes => Objects Array Function
+
+There are two types of memory storage 
+1. Stack (It stores primitive datatype )
+2. Heap 
+
+*/
+
+let myName = "Stuti";
+let anotherName = myName;
+anotherName = "Hello";
+
+// console.log(myName); // Stuti
+// console.log(anotherName); // Hello
+
+// It means when we take referance from primitive datatype it gives copy of the referance not original value
+
+let userOne = {
+  userName: "Stuti ",
+  userEmail: "abc@fg",
+};
+
+let userTwo = userOne;
+
+userTwo.userName = "NewName";
+
+console.log(userOne); //{ userName: 'NewName', userEmail: 'abc@fg' }
+console.log(userTwo); //{ userName: 'NewName', userEmail: 'abc@fg' }
+
+// so above is a object so it do not give the old value it updated the value and gives us the changed value
