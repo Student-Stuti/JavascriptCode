9a64683f216591c9e561dea6595f29797063d409
Student Stuti <stutisharma8912@gmail.com>
2025-01-18 16:09:56 +0530

    added some changes

---
+++ Basics_1/04_string.js
@@ -0,0 +1,16 @@
+const gameName = new String("helloWorld");
+
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+let str = "hello";
+let arr = str.split("");
+console.log(arr);
+
+let str2 = "apple,banana,grapes,orange";
+arr2 = str2.split(",");
+console.log(arr2);
