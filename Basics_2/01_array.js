6dadf40464dd8cf878453c8758d7c4d41afd4717
Student Stuti <stutisharma8912@gmail.com>
2025-01-19 13:53:41 +0530

    added object.js file

---
+++ Basics_2/01_array.js
@@ -0,0 +1,10 @@
+let myArr = [0, 1, 4, 3];
+
+console.log(myArr);
+
+//Array methods
+
+myArr.push(9);
+myArr.pop();
+
+console.log(myArr);
---
+++ Basics_2/02_object.js
@@ -0,0 +1,19 @@
+const mySym = Symbol("key1");
+
+const jsUser = {
+  name: "Stuti",
+  email: "stuti@gmail.com",
+  age: 23,
+  [mySym]: "mykey1",
+  location: "Kangra",
+  isLoggedIn: false,
+  lastLoggedin: ["Monday", "Saturday"],
+};
+
+console.log(jsUser.email); //stuti@gmail.com
+
+console.log(jsUser["email"]); //stuti@gmail.com
+
+console.log(typeof jsUser.mySym); //undefined
+
+console.log(jsUser[mySym]); //mykey1
