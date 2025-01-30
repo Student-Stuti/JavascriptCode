a0206b6a9216cfcfcad3b8c0c449e486eb402a7a
Student Stuti <stutisharma8912@gmail.com>
2025-01-30 13:23:17 +0530

    added in array

--- Basics_2/01_array.js
+++ Basics_2/01_array.js
@@ -2,9 +2,33 @@ let myArr = [0, 1, 4, 3];
 
 console.log(myArr);
 
+//another way of declaring arrays
+
+let myArr2 = new Array(1, 2, 3, 4, 5);
+
 //Array methods
 
 myArr.push(9);
 myArr.pop();
 
-console.log(myArr);
+myArr.unshift(9); //[ 9, 0, 1, 4, 3 ]
+myArr.shift(); //[ 0, 1, 4, 3 ]
+console.log(myArr); //[ 0, 1, 4, 3 ]
+
+myArr2.shift();
+console.log(myArr2); //[ 2, 3, 4, 5 ]
+
+console.log(myArr.includes(3)); //true
+
+console.log(myArr.indexOf(3)); //3
+
+//++++++++++++++++++++++++++++++++JOIN+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+let newArr = myArr.join();
+
+console.log(myArr); //[ 0, 1, 4, 3 ]
+console.log(newArr); //0,1,4,3
+console.log(typeof newArr); //string
+
+
