fa938a05466e1fc1a3f4aa146fbb7993f72ece9f
Student Stuti <stutisharma8912@gmail.com>
2025-02-03 14:23:32 +0530

    added changes

--- Basics_2/02_object.js
+++ Basics_2/02_object.js
@@ -2,6 +2,7 @@ const mySym = Symbol("key1");
 
 const jsUser = {
   name: "Stuti",
+  "fullName": "Stuti Sharma", // there is only one way to access this value jsUser["fullName"]
   email: "stuti@gmail.com",
   age: 23,
   [mySym]: "mykey1",
@@ -12,12 +13,10 @@ const jsUser = {
 
 // console.log(jsUser.email); //stuti@gmail.com
 
+console.log(jsUser["fullName"]);
+
 // console.log(jsUser["email"]); //stuti@gmail.com
 
 // console.log(typeof jsUser.mySym); //undefined
 
 // console.log(jsUser[mySym]); //mykey1
-
-
-
-
